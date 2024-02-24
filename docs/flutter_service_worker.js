444f05ae8661054be26aa686203c75c623ac487e
Jack Lu <18733796+dangkychua@users.noreply.github.com>
2024-02-24 16:37:09 +0700

    temporary remove load env

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -68,9 +68,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "17eb499913c8c70f88023eb26cd7b36e
 "icons/Icon-512.png": "2fd12277dfa797cc40891d3ae36287d9",
 "icons/Icon-maskable-192.png": "7964370e4843436f66417007bfd0d3ad",
 "icons/Icon-maskable-512.png": "2fd12277dfa797cc40891d3ae36287d9",
-"index.html": "e701043730a09ecf6d5e8afc2f4e6391",
-"/": "e701043730a09ecf6d5e8afc2f4e6391",
-"main.dart.js": "13da5669ade290efdb884ef79b6dd201",
+"index.html": "3818b2c17434ddfca0367fc9d261655f",
+"/": "3818b2c17434ddfca0367fc9d261655f",
+"main.dart.js": "4a777d395c69af08b3cf27e7a83b3086",
 "manifest.json": "5fd3357b6a26a1e6c92d14efc2c92471",
 "version.json": "16676f095e684d85215d3b557a6e0206"};
 // The application shell files that are downloaded before a service worker can
